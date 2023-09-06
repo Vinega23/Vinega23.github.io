ff8b8b2d5842a20072e05b4bfd83d756017c5184
Vinega23 <114490489+Vinega23@users.noreply.github.com>
2023-09-06 20:17:21 +0200

    Galerie - added lightbox2 plug-in

--- js/script.js
+++ js/script.js
@@ -1,60 +1,24 @@
 (function($){
 
-    var boxImage = $('.box-image');               
+    $(document).ready(function() {        
+        lightbox.option({
+            'resizeDuration': 200,
+            'wrapAround': true,
+            'showImageNumberLabel': true,
+            'imageFadeDuration:': 2000,
+            'fitImagesInViewport': true
+        });
+    });                
     
-    var overlay = $('<div/>', {id: 'overlay' }); 
-    image = $('<img>'); // vytvoreni noveho elementu/image ze zdroje promenne href               
-    overlay.appendTo('body').hide();    //bez metody hide() by byl overlay nacteny pri spusteni stranky
+    var gallery = $('.galerie');
+    startingOpacity = gallery.find('img').css('opacity');
 
-    function efect(neco){
-    neco.fadeOut(1000);
-    };    
+    gallery.find('img').on('mouseenter mouseleave', function(event){
 
-    //kliknuti na obrazek a zobrazeni overlay    
-    boxImage.find('img').on('click', function(event){        
-                        
-        $(image).attr({src: $(this).attr('href')});  //vytahnuti linku odkazu (obrazku)     
-        overlay.html( image ) //zapsani do html overlay promenne image uz s novym obrazkem
-                .fadeIn(1000)
-            //.show();
-            
-        event.preventDefault();       
-    });     
-
-
-    //skryjeme overlay
-    overlay.on('click', function(){
-        efect(overlay);          
-        //.hide()    
-    });
-    //skryjeme overlay po stiknuti ESC, 
-    $(document).on('keyup', function(event){
-        //console.log(event.which); //pomoci console.log muzeme zjistit cislo stiknute klavesy
-        if  (event.which === 27 ) efect(overlay);
-    });
+        var opacity = event.type === 'mouseenter' ? 1 : startingOpacity;
     
-
-    var gallery = $('.galerie'); //vytazeni elementu
-    startingOpacity = gallery.find('img').css('opacity'); //vytazeni hodnoty opacity z CSS
-
-    gallery.find('img').on('mouseenter mouseleave', function(event){ //dva eventy v jednom naslouchaci
-
-        var opacity = event.type === 'mouseenter' ? 1 : startingOpacity; //ternarni operator
-    
-        $(this).stop().fadeTo(200, opacity) //metoda
-    })
-
-    //Overlay galerie
-    var galerie = $('.galerie');
-    galerie.find('a').on('click', function(event){        
-                        
-        $(image).attr({src: $(this).attr('href')});  //vytahnuti linku odkazu (obrazku)     
-        overlay.html( image ) //zapsani do html overlay promenne image uz s novym obrazkem
-                .fadeIn(1000)
-            //.show();
-            
-        event.preventDefault();       
-    });     
+        $(this).stop().fadeTo(200, opacity)
+    })  
 
 
 })(jQuery);
