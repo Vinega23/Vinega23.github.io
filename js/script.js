ab9a997cb4586b96c01fbc3d9b614c26b75a4165
Vinega23 <114490489+Vinega23@users.noreply.github.com>
2023-09-21 21:11:29 +0200

    new and final version of navigation, lightbox.

--- js/script.js
+++ js/script.js
@@ -1,24 +1,49 @@
-(function($){
-
-    $(document).ready(function() {        
-        lightbox.option({
-            'resizeDuration': 200,
-            'wrapAround': true,
-            'showImageNumberLabel': true,
-            'imageFadeDuration:': 2000,
-            'fitImagesInViewport': true
-        });
-    });                
+(function($){                 
     
-    var gallery = $('.galerie');
-    startingOpacity = gallery.find('img').css('opacity');
+    var galleryOne = $('.gallery');
+    startingOpacity = galleryOne.find('img').css('opacity');
 
-    gallery.find('img').on('mouseenter mouseleave', function(event){
+    galleryOne.find('img').on('mouseenter mouseleave', function(event){
 
         var opacity = event.type === 'mouseenter' ? 1 : startingOpacity;
     
         $(this).stop().fadeTo(200, opacity)
-    })  
+    })
 
+    var title = $('.dyazart-title');        
+    title.hide();
+    // $('.dyazart-title')
+    // .hide()
+    // .fadeIn(3000);
 
+    var win = $(window);
+    win.on('scroll', function(){
+        if (win.scrollTop() >=100 ) title.fadeIn(2000); //element se objevi az po scrolovani na pozici 500
+        
+    }) 
+    win.on('scroll', function(){
+        if (win.scrollTop() >=100 ){
+            $('.main-nav').addClass('scrolled');         
+
+        }
+        else {
+            $('.main-nav').removeClass('scrolled');         
+           
+        }
+    }); 
+     
+    var gallery = $('.gallery a').simpleLightbox({    
+          
+            spinner:true,
+            captions:false
+             
+    });
+
+   
+   
+        
+    
+    
+        
 })(jQuery);
+
