f368978a0347f254026dfc4bcccdee4bbe8648df
Vinega23 <vitecek.pe@gmail.com>
2024-04-01 18:55:19 +0200

    new update, some design details changed

--- js/script.js
+++ js/script.js
@@ -1,32 +1,40 @@
-(function($){
-   
-    var galleryOne = $('.gallery');
-    startingOpacity = galleryOne.find('img').css('opacity');
+(function ($) {
+  var galleryOne = $(".gallery");
+  startingOpacity = galleryOne.find("img").css("opacity");
 
-    galleryOne.find('img').on('mouseenter mouseleave', function(event){
+  galleryOne.find("img").on("mouseenter mouseleave", function (event) {
+    var opacity = event.type === "mouseenter" ? 1 : startingOpacity;
 
-        var opacity = event.type === 'mouseenter' ? 1 : startingOpacity;
-    
-        $(this).stop().fadeTo(200, opacity)
-    });    
-    
-    function scrollAndRoll(position){
-        return win.scrollTop() >= position;
-    };
-    var title = $('.dyazart-title');
-    
-    
-    $('#menuToggle ul li a').on('click',function() {      
-      $('#op').prop('checked', false);
-    });
-       
-    var gallery = $('.gallery a').simpleLightbox({    
-          
-            spinner:true,
-            captions:false
-             
-    });
-   
-  
-   
+    $(this).stop().fadeTo(200, opacity);
+  });
+
+  function scrollAndRoll(position) {
+    return win.scrollTop() >= position;
+  }
+  var title = $(".dyazart-title");
+
+  $("#menuToggle ul li a").on("click", function () {
+    $("#op").prop("checked", false);
+  });
+
+  var gallery = $(".gallery a").simpleLightbox({
+    spinner: true,
+    captions: false,
+  });
+  // ===== Scroll to Top ====
+  $(window).scroll(function () {
+    if ($(this).scrollTop() >= 200) {
+      $("#return-to-top").fadeIn(250);
+    } else {
+      $("#return-to-top").fadeOut(250);
+    }
+  });
+  $("#return-to-top").click(function () {
+    $("body,html").animate(
+      {
+        scrollTop: 0,
+      },
+      500
+    );
+  });
 })(jQuery);
